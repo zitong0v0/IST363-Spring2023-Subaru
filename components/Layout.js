f4756ddbc961a188a397a1a37355eec63d0a0740
zitong0v0 <samanthahuang0000@gmail.com>
2023-03-21 17:38:28 -0400

    first commit

---
+++ components/Layout.js
@@ -0,0 +1,31 @@
+import { Fragment } from "react"
+
+import Header from './Header';
+import Footer from './Footer';
+import Link from 'next/link';
+
+const Layout = ({children}) => {
+
+    return <Fragment>
+        <Header />
+        <nav>
+            <ul>
+                <li>
+                    <Link href="/">
+                    Home
+                    </Link>
+                </li>
+                <li>
+                    <Link href="/products">
+                    Products
+                    </Link>
+                </li>
+            </ul>
+        </nav>
+        <main>
+            {children}
+        </main>
+        <Footer />
+    </Fragment>
+}
+export default Layout
---
+++ components/NavOverlay.js
@@ -0,0 +1,13 @@
+import Button from './Button';
+import styles from './navoverlay.module.css';
+
+const NavOverlay = ({closeHandler}) => {
+    return <div className={styles.navoverlay}>
+        <Button
+            label="Close"
+            clickHandler={closeHandler}
+        />
+        NavOverlay goes here.
+    </div>
+}
+export default NavOverlay
