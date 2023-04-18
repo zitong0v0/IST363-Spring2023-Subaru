64cbd05c01116ac212f717964b31f320dcec6a35
zitong0v0 <samanthahuang0000@gmail.com>
2023-04-18 18:20:15 -0400

    configgured route

--- pages/index.js
+++ pages/index.js
@@ -8,33 +8,44 @@ import Showcase from '../components/Showcase';
 import SectionCTA from '../components/SectionCTA';
 
 
-import { getAllVehicles, getAllColors } from '../lib/api';
+import { 
+  //getAllVehicles, 
+  //getAllColors 
+} from '../lib/api';
 
 
 export async function getStaticProps() {
   // Get external data from the file system, API, DB, etc.
-  const data = getAllVehicles();
-  const colors = getAllColors();
+  //const data = getAllVehicles();
+  //const colors = getAllColors();
 
   // The value of the `props` key will be
   //  passed to the `Home` component
 
   return {
     props: {
-      data,
-      colors
+      //data,
+      //colors
     }
   } 
 }
 
-const Homepage = ({data, colors}) => {
+const Homepage = ({
+  //data, 
+  //colors
+}) => {
   //console.log({data});
   return <Layout>
-    <Showcase />
-    <LocationPicker />
-    <CarColorPicker colors={colors}/>
-    <SectionCTA />
-
+    {
+      //<Showcase />
+      //<LocationPicker />
+    
+      //<CarColorPicker colors={colors}/>
+      //<SectionCTA />
+    }
+    
+   
+    
   </Layout>
 }
 export default Homepage
